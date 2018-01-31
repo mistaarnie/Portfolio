95202e6e1cf754725966506b3a9a39f5ed064b67
Arnold Chang <mistaarnie@users.noreply.github.com>
2018-01-30 16:41:01 -0800

    Portfolio V2

--- js/main.js
+++ js/main.js
@@ -13,209 +13,32 @@ $('.button').on('click', function(event) {
   }
 });
 
-/** Menu **/
-jQuery(document).ready(function($){
-	var mainHeader = $('.nav-container'),
-		belowNavHeroContent = $('.sub-nav-hero'),
-		headerHeight = mainHeader.height();
-
-	//set scrolling variables
-	var scrolling = false,
-		previousTop = 0,
-		currentTop = 0,
-		scrollDelta = 10,
-		scrollOffset = 130;
-
-	mainHeader.on('click', '.nav-trigger', function(event){
-		// open primary navigation on mobile
-		event.preventDefault();
-        $('.inner').toggleClass('transform-inner');
-		mainHeader.toggleClass('nav-open');
-        $('.black-overlay').toggleClass('show-black-overlay');
-        
-	});
-  
-    mainHeader.on('click', '.primary-nav ul li a', function(event){
-      mainHeader.toggleClass('nav-open');
-      $('.inner').toggleClass('transform-inner');
-      $('.black-overlay').toggleClass('show-black-overlay');
-      
-      var hash = this.hash;
-
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-        }, 800, function() {
-            window.location.hash = hash;
-      });
-      
-    });
-
-	$(window).on('scroll', function(){
-		if( !scrolling ) {
-			scrolling = true;
-			(!window.requestAnimationFrame)
-				? setTimeout(autoHideHeader, 250)
-				: requestAnimationFrame(autoHideHeader);
-		}
-	});
-
-	$(window).on('resize', function(){
-		headerHeight = mainHeader.height();
-	});
-
-	function autoHideHeader() {
-		var currentTop = $(window).scrollTop();
-
-		( belowNavHeroContent.length > 0 )
-			? checkStickyNavigation(currentTop) // secondary navigation below intro
-			: checkSimpleNavigation(currentTop);
-
-	   	previousTop = currentTop;
-		scrolling = false;
-	}
-
-	function checkSimpleNavigation(currentTop) {
-		//there's no secondary nav or secondary nav is below primary nav
-	    if ((previousTop - currentTop > scrollDelta) || ($('.inner').hasClass('transform-inner'))) {
-	    	//if scrolling up...
-	    	mainHeader.removeClass('is-hidden');
-	    } else if( currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
-	    	//if scrolling down...
-	    	mainHeader.addClass('is-hidden');
-	    }
-	}
-
-	function checkStickyNavigation(currentTop) {
-		//secondary nav below intro section - sticky secondary nav
-		var secondaryNavOffsetTop = belowNavHeroContent.offset().top - secondaryNavigation.height() - mainHeader.height();
-
-		if (previousTop >= currentTop ) {
-	    	//if scrolling up...
-	    	if( currentTop < secondaryNavOffsetTop ) {
-	    		//secondary nav is not fixed
-	    		mainHeader.removeClass('is-hidden');
-	    	} else if( previousTop - currentTop > scrollDelta ) {
-	    		//secondary nav is fixed
-	    		mainHeader.removeClass('is-hidden');
-	    	}
-
-	    } else {
-	    	//if scrolling down...
-	 	  	if( currentTop > secondaryNavOffsetTop + scrollOffset ) {
-	 	  		//hide primary nav
-	    		mainHeader.addClass('is-hidden');
-                mainHeader.toggleClass('nav-open');
-	    	} else if( currentTop > secondaryNavOffsetTop ) {
-	    		//once the secondary nav is fixed, do not hide primary nav if you haven't scrolled more than scrollOffset
-	    		mainHeader.removeClass('is-hidden');
-	    	}
-
-	    }
-	}
-
-});
-
-
-/*** chat box created by Andre Madarang https://codepen.io/drehimself/ ***/
-(function(){
-
-  var chat = {
-    messageToSend: '',
-    messageResponses: [
-      'What time do Asians take showers? At too-dirty. 🕑',
-      'Why would grapes make good parents? Cause they\'re good at raisin their children. 🍇',
-      'Why do cats have the highest chance of getting into heaven? Cause they have puuuurity. 🐱',
-      'What group of animals are the most afraid? A cow-herd. 🐮'
-    ],
-    init: function() {
-      this.cacheDOM();
-      this.bindEvents();
-      this.render();
-    },
-    cacheDOM: function() {
-      this.$chatHistory = $('.chat-history');
-      this.$button = $('.send-btn');
-      this.$textarea = $('#message-to-send');
-      this.$chatHistoryList =  this.$chatHistory.find('ul');
-    },
-    bindEvents: function() {
-      this.$button.on('click', this.addMessage.bind(this));
-      this.$textarea.on('keyup', this.addMessageEnter.bind(this));
-    },
-    render: function() {
-      this.scrollToBottom();
-      if (this.messageToSend.trim() !== '') {
-        var template = Handlebars.compile( $("#message-template").html());
-        var context = {
-          messageOutput: this.messageToSend,
-          time: this.getCurrentTime()
-        };
-
-        this.$chatHistoryList.append(template(context));
-        this.scrollToBottom();
-        this.$textarea.val('');
-
-        // responses
-        var templateResponse = Handlebars.compile( $("#message-response-template").html());
-        var contextResponse = {
-          response: this.getRandomItem(this.messageResponses),
-          time: this.getCurrentTime()
-        };
-
-        setTimeout(function() {
-          this.$chatHistoryList.append(templateResponse(contextResponse));
-          this.scrollToBottom();
-        }.bind(this), 1500);
-
-      }
-
-    },
-
-    addMessage: function() {
-      this.messageToSend = this.$textarea.val()
-      this.render();
-    },
-    addMessageEnter: function(event) {
-        // enter was pressed
-        if (event.keyCode === 13) {
-          this.addMessage();
-        }
-    },
-    scrollToBottom: function() {
-       this.$chatHistory.scrollTop(this.$chatHistory[0].scrollHeight);
-    },
-    getCurrentTime: function() {
-      return new Date().toLocaleTimeString().
-              replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3");
-    },
-    getRandomItem: function(arr) {
-      return arr[Math.floor(Math.random()*arr.length)];
-    }
-
-  };
-
-  chat.init();
-
-  var searchFilter = {
-    options: { valueNames: ['name'] },
-    init: function() {
-      var userList = new List('people-list', this.options);
-      var noItems = $('<li id="no-items-found">No items found</li>');
-
-      userList.on('updated', function(list) {
-        if (list.matchingItems.length === 0) {
-          $(list.list).append(noItems);
-        } else {
-          noItems.detach();
-        }
-      });
-    }
-  };
-
-  searchFilter.init();
-
-})();
 
 AOS.init({
-  duration: 1800
+  duration: 1000
+});
+
+$(document).ready(function() {
+  $(".animsition").animsition({
+    inClass: 'fade-in',
+    outClass: 'fade-out',
+    inDuration: 1000,
+    outDuration: 500,
+    linkElement: '.animsition-link',
+    // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+    loading: true,
+    loadingParentElement: 'body', //animsition wrapper element
+    loadingClass: 'animsition-loading',
+    loadingInner: '', // e.g '<img src="loading.svg" />'
+    timeout: false,
+    timeoutCountdown: 5000,
+    onLoadEvent: true,
+    browser: [ 'animation-duration', '-webkit-animation-duration'],
+    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+    overlay : false,
+    overlayClass : 'animsition-overlay-slide',
+    overlayParentElement : 'body',
+    transition: function(url){ window.location.href = url; }
+  });
 });
